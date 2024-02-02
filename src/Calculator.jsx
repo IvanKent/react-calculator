984b0d0255c506ee83432b9cfda3ad92eeb2030d
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-02-02 08:21:14 +0800

    'added_basic_calculator_styles'

---
+++ src/Calculator.jsx
@@ -0,0 +1,32 @@
+import './calculator.css'
+export default function Calculator(){
+    return(
+        <div className="calcContainer">
+            <div className="calcRowContainer">
+                <button>1</button>
+                <button>1</button>
+                <button>1</button>
+                <button>1</button>
+            </div>
+            <div className="calcRowContainer">
+                <button>1</button>
+                <button>1</button>
+                <button>1</button>
+                <button>1</button>
+            </div>
+            <div className="calcRowContainer">
+                <button>1</button>
+                <button>1</button>
+                <button>1</button>
+                <button>1</button>
+            </div>
+            <div className="calcRowContainer">
+                <button>1</button>
+                <button>1</button>
+                <button>1</button>
+                <button>1</button>
+            </div>
+        </div>
+        
+    )
+}
